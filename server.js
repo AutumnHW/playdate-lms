afecc46a56a08dca29188d114bd5115d9f865347
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2026-07-07 01:32:09 -0400

    attempting db connections

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require('express');
+import 'dotenv/config';
+import express from 'express';
+import pg from 'pg';
+
+const { Client } = pg;
+
 const app = express();
+const client = new Client();
+
+await client.connect();
 //todo: add a case in case a custom port is defined in the environment variables.
 const port = 41511;
 
+
+
+console.log(`postgresdb running on ${process.env.PGHOST}`)
+console.log(`postgresdb running on port ${process.env.PGPORT}`)
+console.log(await client.query('SELECT * FROM cars;'))
 
 // Respond to GET request on the root route
 app.get('/', (req, res) => {
@@ -27,4 +40,5 @@ app.use((req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`started playdate at http://localhost:${port}`);
+
 }); 
